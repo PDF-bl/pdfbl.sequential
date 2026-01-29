d1f9ad1e34328ada13ab3e79883cf0bdf634f173
Yuchen Xiao <yx2924@columbia.edu>
2026-01-29 12:42:51 -0500

    style: use `pytest` instead of `unittest.TestCase`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,40 @@
-import json
 from pathlib import Path
 
 import pytest
 
+from pdfbl.interface import FitDAG, FitRunner
+
 
 @pytest.fixture
-def user_filesystem(tmp_path):
-    base_dir = Path(tmp_path)
-    home_dir = base_dir / "home_dir"
-    home_dir.mkdir(parents=True, exist_ok=True)
-    cwd_dir = base_dir / "cwd_dir"
-    cwd_dir.mkdir(parents=True, exist_ok=True)
-
-    home_config_data = {"username": "home_username", "email": "home@email.com"}
-    with open(home_dir / "diffpyconfig.json", "w") as f:
-        json.dump(home_config_data, f)
-
-    yield tmp_path
+def fitrunnner_related_instances_dict():
+    profile_path = Path("tests/data/Ni.gr")
+    structure_path = Path("tests/data/Ni.cif")
+    inputs = {
+        "profile_string": profile_path.read_text(),
+        "structure_string": structure_path.read_text(),
+        "xmin": 1.5,
+        "xmax": 50,
+        "dx": 0.01,
+        "qmax": 25.0,
+        "qmin": 0.1,
+        "remove_vars": ["delta1"],
+    }
+    payload = {
+        "scale": 0.4,
+        "a": 3.52,
+        "Uiso_0": 0.005,
+        "delta2": 2.0,
+        "qdamp": 0.04,
+        "qbroad": 0.02,
+    }
+    dag = FitDAG()
+    dag.from_str("a->scale->qdamp->Uiso_0->delta2->all")
+    runner = FitRunner()
+    yield {
+        "runner": runner,
+        "dag": dag,
+        "payload": payload,
+        "inputs": inputs,
+        "structure_path": structure_path,
+        "profile_path": profile_path,
+    }
--- tests/test_fitrunner.py
+++ tests/test_fitrunner.py
@@ -1,7 +1,7 @@
 import sys
-import unittest
 from pathlib import Path
 
+import numpy
 from scipy.optimize import least_squares
 
 from pdfbl.interface import FitDAG, FitRunner, PDFAdapter
@@ -10,126 +10,92 @@ sys.path.append(str(Path(__file__).parent / "diffpycmi_scripts.py"))
 from diffpycmi_scripts import make_recipe  # noqa: E402
 
 
-class TestFitRunner(unittest.TestCase):
-    def setUp(self):
-        profile_path = Path("tests/data/Ni.gr")
-        structure_path = Path("tests/data/Ni.cif")
-        inputs = {
-            "profile_string": profile_path.read_text(),
-            "structure_string": structure_path.read_text(),
-            "xmin": 1.5,
-            "xmax": 50,
-            "dx": 0.01,
-            "qmax": 25.0,
-            "qmin": 0.1,
-            "remove_vars": ["delta1"],
-        }
-        payload = {
-            "scale": 0.4,
-            "a": 3.52,
-            "Uiso_0": 0.005,
-            "delta2": 2.0,
-            "qdamp": 0.04,
-            "qbroad": 0.02,
-        }
-        dag = FitDAG()
-        dag.from_str("a->scale->qdamp->Uiso_0->delta2->all")
-        runner = FitRunner()
-        self.runner = runner
-        self.dag = dag
-        self.payload = payload
-        self.inputs = inputs
-        self.profile_path = profile_path
-        self.structure_path = structure_path
-
-    def test_run(self):
-        # C1: Run the same Fit using diffpy.cmi and FitRunner.
-        #  Expect to get similar results.
-        diffpycmi_recipe = make_recipe(
-            str(self.structure_path), str(self.profile_path)
+def test_run(fitrunnner_related_instances_dict):
+    structure_path = fitrunnner_related_instances_dict["structure_path"]
+    profile_path = fitrunnner_related_instances_dict["profile_path"]
+    inputs = fitrunnner_related_instances_dict["inputs"]
+    payload = fitrunnner_related_instances_dict["payload"]
+    # C1: Run the same Fit using diffpy.cmi and FitRunner.
+    #  Expect to get similar results.
+    diffpycmi_recipe = make_recipe(str(structure_path), str(profile_path))
+    diffpycmi_recipe.fithooks[0].verbose = 0
+    diffpycmi_recipe.fix("all")
+    tags = ["lat", "scale", "adp", "d2", "all"]
+    for tag in tags:
+        diffpycmi_recipe.free(tag)
+        least_squares(
+            diffpycmi_recipe.residual,
+            diffpycmi_recipe.values,
+            x_scale="jac",
         )
-        diffpycmi_recipe.fithooks[0].verbose = 0
-        diffpycmi_recipe.fix("all")
-        tags = ["lat", "scale", "adp", "d2", "all"]
-        for tag in tags:
-            diffpycmi_recipe.free(tag)
-            least_squares(
-                diffpycmi_recipe.residual,
-                diffpycmi_recipe.values,
-                x_scale="jac",
-            )
-        diffpynames = [
-            "fcc_Lat",
-            "s1",
-            "fcc_ADP",
-            "Ni_Delta2",
-            "Calib_Qdamp",
-            "Calib_Qbroad",
-            "all",
-        ]
-        pname = ["a", "scale", "Uiso_0", "delta2", "qdamp", "qbroad", "all"]
-        diffpy2pname = {
-            diffpyname: pname[i] for i, diffpyname in enumerate(diffpynames)
-        }
-        diffpy_pv_dict = {}
-        for tag, parameter in diffpycmi_recipe._parameters.items():
-            pname = diffpy2pname[tag]
-            diffpy_pv_dict[pname] = parameter.value
+    diffpyname_to_runner_name = {
+        "fcc_Lat": "a",
+        "s1": "scale",
+        "fcc_ADP": "Uiso_0",
+        "Ni_Delta2": "delta2",
+        "Calib_Qdamp": "qdamp",
+        "Calib_Qbroad": "qbroad",
+        "all": "all",
+    }
+    diffpy_pv_dict = {}
+    for tag, parameter in diffpycmi_recipe._parameters.items():
+        pname = diffpyname_to_runner_name[tag]
+        diffpy_pv_dict[pname] = parameter.value
+    dag = FitDAG()
+    dag.from_str("a->scale->qdamp->Uiso_0->delta2->all")
+    runner = FitRunner()
+    runner._run_dag(dag, PDFAdapter, inputs, payload)
+    last_node_id = dag.leaf_nodes[0]
+    last_node = dag.nodes[last_node_id]
+    runner_pv_dict = {
+        pname: last_node["payload"][pname] for pname in diffpy_pv_dict.keys()
+    }
+    for pname in diffpy_pv_dict.keys():
+        numpy.isclose(diffpy_pv_dict[pname], runner_pv_dict[pname], rtol=1e-5)
 
-        dag = FitDAG()
-        dag.from_str("a->scale->qdamp->Uiso_0->delta2->all")
-        runner = FitRunner()
-        # Inputs and payload are the same as those used in the diffpy.cmi
-        runner._run_dag(dag, PDFAdapter, self.inputs, self.payload)
-        last_node_id = dag.leaf_nodes[0]
-        last_node = dag.nodes[last_node_id]
-        runner_pv_dict = {
-            pname: last_node["payload"][pname]
-            for pname in diffpy_pv_dict.keys()
-        }
 
-        for pname in diffpy_pv_dict.keys():
-            self.assertAlmostEqual(
-                diffpy_pv_dict[pname], runner_pv_dict[pname], places=4
-            )
+def test_watch(fitrunner_related_instances_dict):
+    # C1: Collect data for parameter "a" at the end of each node
+    #  Expect 6 data points collected for parameter "a"
+    runner = fitrunner_related_instances_dict["runner"]
+    dag = fitrunner_related_instances_dict["dag"]
+    inputs = fitrunner_related_instances_dict["inputs"]
+    payload = fitrunner_related_instances_dict["payload"]
+    runner.watch(
+        lambda dag, node_id: True,
+        pname="a",
+        update_mode="append",
+        source="payload",
+    )
+    runner._run_dag(dag, PDFAdapter, inputs, payload)
+    assert list(runner.data_for_plot.values())[0]["ydata"].qsize() == 6
 
-    def test_watch(self):
-        # C1: Collect data for parameter "a" from the payload of each node
-        #  Expect 6 data points collected for parameter "a"
-        self.runner.watch(
-            lambda dag, node_id: True,
-            pname="a",
-            update_mode="append",
-            source="payload",
-        )
-        self.runner._run_dag(self.dag, PDFAdapter, self.inputs, self.payload)
-        self.assertEqual(
-            list(self.runner.data_for_plot.values())[0]["ydata"].qsize(), 6
-        )
 
-    def test_mark(self):
-        # C1: Mark a node "hasAdapter" and "hasPayload"
-        #  Expect runner.is_marked(node_id, "initialized") to be True
-        node_id = list(self.dag.nodes.keys())[0]
-        self.runner.mark(node_id, "hasAdapter")
-        self.runner.mark(node_id, "hasPayload")
-        self.assertTrue(self.runner.is_marked(node_id, "initialized"))
-        # C2: Mark a node "hasAdapter" only
-        #  Expect runner.is_marked(node_id, "initialized") to be False
-        node_id = list(self.dag.nodes.keys())[1]
-        self.runner.mark(node_id, "hasAdapter")
-        self.assertFalse(self.runner.is_marked(node_id, "initialized"))
-        # C3: Mark a node "hasPayload" only
-        #  Expect runner.is_marked(node_id, "initialized") to be False
-        node_id = list(self.dag.nodes.keys())[2]
-        self.runner.mark(node_id, "hasPayload")
-        self.assertFalse(self.runner.is_marked(node_id, "initialized"))
-        # C4: Mark a node "hasAdapter", "hasPayload" and "completed"
-        #  Expect runner.is_marked(node_id, "initialized") to be False
-        #  runner.is_marked(node_id, "initialized") to be True
-        node_id = list(self.dag.nodes.keys())[3]
-        self.runner.mark(node_id, "hasAdapter")
-        self.runner.mark(node_id, "hasPayload")
-        self.runner.mark(node_id, "completed")
-        self.assertFalse(self.runner.is_marked(node_id, "initialized"))
-        self.assertTrue(self.runner.is_marked(node_id, "completed"))
+def test_mark(fitrunner_related_instances_dict):
+    runner = fitrunner_related_instances_dict["runner"]
+    dag = fitrunner_related_instances_dict["dag"]
+    # C1: Mark a node "hasAdapter" and "hasPayload"
+    #  Expect runner.is_marked(node_id, "initialized") to be True
+    node_id = list(dag.nodes.keys())[0]
+    runner.mark(node_id, "hasAdapter")
+    runner.mark(node_id, "hasPayload")
+    assert runner.is_marked(node_id, "initialized")
+    # C2: Mark a node "hasAdapter" only
+    #  Expect runner.is_marked(node_id, "initialized") to be False
+    node_id = list(dag.nodes.keys())[1]
+    runner.mark(node_id, "hasAdapter")
+    assert not runner.is_marked(node_id, "initialized")
+    # C3: Mark a node "hasPayload" only
+    #  Expect runner.is_marked(node_id, "initialized") to be False
+    node_id = list(dag.nodes.keys())[2]
+    runner.mark(node_id, "hasPayload")
+    assert not runner.is_marked(node_id, "initialized")
+    # C4: Mark a node "hasAdapter", "hasPayload" and "completed"
+    #  Expect runner.is_marked(node_id, "initialized") to be False
+    #  runner.is_marked(node_id, "initialized") to be True
+    node_id = list(dag.nodes.keys())[3]
+    runner.mark(node_id, "hasAdapter")
+    runner.mark(node_id, "hasPayload")
+    runner.mark(node_id, "completed")
+    assert not runner.is_marked(node_id, "initialized")
+    assert runner.is_marked(node_id, "completed")
